5118fe8dc22c3e95670db699f9d93c6ef23567d6
Raul <r.vargas@tesipro.com>
2018-10-15 12:57:56 +0200

    Commit Inicial

---
+++ app.js
@@ -0,0 +1,34 @@
+const argv = require('./config/yargs').argv;
+
+
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: ${ archivo }`))
+            .catch(e => console.log(e));
+        break;
+
+    default:
+        console.log('comando no reconocido');
+}
+
+//let base = '5';
+
+//console.log(process.argv);
+
+let argv2 = process.argv;
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
+
+// console.log( argv.base);
+console.log(argv);
